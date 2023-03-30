9989c206682664c4beed1fe5c6f7139022906c02
Akansha Srivastava <akansha.srivastava@vida.id>
2023-03-30 15:02:15 +0530

    swift file added

---
+++ SwiftLib/SwiftLib.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftLib.swift
+//  SwiftLib
+//
+//  Created by Akansha Srivastava on 30/03/23.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
